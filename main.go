f47ff6de9616b6c026fe634a6cb3313f83a7c2c9
Arpit <arpit@Arpits-MacBook-Air.local>
2022-10-08 14:22:39 +0530

    update channels

--- main.go
+++ main.go
@@ -2,10 +2,8 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"io/ioutil"
 	"net/http"
-	"time"
 )
 
 var (
@@ -16,6 +14,7 @@ var (
 		"amazon.com",
 		"github.com",
 		"gitlab.com",
+		"bitnz.com",
 	}
 
 	results struct {
@@ -27,40 +26,64 @@ var (
 	}
 )
 
+type ItemResult struct {
+	Url           string
+	ContentLength int
+}
+
 func main() {
-	fmt.Println("Arpit")
-	fmt.Println(webPages)
-	start := time.Now()
-	ch := make(chan string)
+	// create an http client, a list of urls and a channel to
+	// pass url fetch results back
+	client := new(http.Client)
+	ch := make(chan *ItemResult)
 
+	results.ContentLength = make(map[string]int)
+
+	// fetch the contents of the list of urls in parallel
 	for i := 0; i < len(webPages); i++ {
-		go fetch(webPages[i], ch)
-	}
-	for i := 0; i < len(webPages); i++ {
-		fmt.Println(<-ch)
+		go fetch(client, webPages[i], ch)
 	}
 
-	fmt.Println(results)
-	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+	// print out the results as soon as they come in
+	//  (here we could write the data to disk if required)
+	for i := 0; i < len(webPages); i++ {
+		output := <-ch
+		fmt.Printf("%s,\t%d bytes\n", output.Url, output.ContentLength)
+		results.ContentLength[output.Url] = output.ContentLength
+		results.TotalContentLength = results.TotalContentLength + output.ContentLength
+	}
+
+	fmt.Print(results)
 
 }
 
-func fetch(url string, ch chan<- string) {
-	fmt.Println(url)
+func fetch(client *http.Client, url string, ch chan *ItemResult) {
+	result := ItemResult{url, -1}
 
-	start := time.Now()
-	resp, err := http.Get("https://" + url)
+	// get the url
+	response, err := client.Get("https://" + url)
 	if err != nil {
-		ch <- fmt.Sprint(err) // send to channel ch
+		fmt.Println(err, url)
+		ch <- &result
 		return
 	}
-	nbytes, err := io.Copy(ioutil.Discard, resp.Body)
-	resp.Body.Close() // don't leak resources
-	if err != nil {
-		ch <- fmt.Sprintf("while reading %s: %v", url, err)
+
+	// check the http server response was OK
+	if response.Status != "200 OK" {
+		fmt.Println(response.Status, url)
+		ch <- &result
 		return
 	}
-	secs := time.Since(start).Seconds()
-	ch <- fmt.Sprintf("%.2fs %7d %s", secs, nbytes, url)
 
+	// read the response body
+	b, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		fmt.Println(err, url)
+		ch <- &result
+		return
+	}
+
+	// pass the result back to caller via a channel
+	result.ContentLength = len(string(b))
+	ch <- &result
 }
